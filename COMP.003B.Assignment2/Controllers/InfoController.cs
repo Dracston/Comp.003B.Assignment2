fe00d1aaebeeec517bc669a48c2f4a46ee9f9bd4
Dracston <lpj11101@gmail.com>
2024-04-01 20:14:36 -0700

    Add project files.

---
+++ COMP.003B.Assignment2/Controllers/InfoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Comp._003B.Assignment2.Controllers
+{
+    public class InfoController : Controller
+    {
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Hobbies() 
+        {
+            return View();
+        }
+    }
+}
---
+++ COMP.003B.Assignment2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Comp._003B.Assignment2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
